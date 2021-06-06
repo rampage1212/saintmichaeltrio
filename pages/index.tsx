2379baf5ae8ecd624a51dac2c2294f76af06bf65
rampage0131 <jake.docker0131@gmail.com>
2021-06-06 21:39:35 +0000

    feat: use strict-mode TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
+import styles from 'styles/Home.module.css'
+
+export default function Home(): JSX.Element {
   return (
     <div className={styles.container}>
       <Head>
