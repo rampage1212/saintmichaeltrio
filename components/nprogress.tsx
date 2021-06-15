8236816223be38604ac552ffc47e0569819880b7
rampage0131 <jake.docker0131@gmail.com>
2021-06-15 23:50:18 +0000

    feat(nprogress): remove spinning indicator

--- components/nprogress.tsx
+++ components/nprogress.tsx
@@ -48,52 +48,6 @@ export default function Progress(): JSX.Element {
         -ms-transform: rotate(3deg) translate(0px, -4px);
         transform: rotate(3deg) translate(0px, -4px);
       }
-
-      #nprogress .spinner {
-        display: block;
-        position: fixed;
-        z-index: 100;
-        bottom: 15px;
-        right: 15px;
-        opacity: 1;
-        will-change: opacity;
-        transition: opacity 0.2s ease-in-out;
-      }
-
-      .intercom #nprogress .spinner {
-        opacity: 0;
-      }
-
-      #nprogress .spinner-icon {
-        width: 18px;
-        height: 18px;
-        box-sizing: border-box;
-
-        border: solid 2px transparent;
-        border-top-color: var(--primary);
-        border-left-color: var(--primary);
-        border-radius: 50%;
-
-        -webkit-animation: nprogress-spinner 400ms linear infinite;
-        animation: nprogress-spinner 400ms linear infinite;
-      }
-
-      @-webkit-keyframes nprogress-spinner {
-        0% {
-          -webkit-transform: rotate(0deg);
-        }
-        100% {
-          -webkit-transform: rotate(360deg);
-        }
-      }
-      @keyframes nprogress-spinner {
-        0% {
-          transform: rotate(0deg);
-        }
-        100% {
-          transform: rotate(360deg);
-        }
-      }
     `}</style>
   );
 }
