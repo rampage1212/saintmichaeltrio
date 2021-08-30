431bb29980a45d6142e03a1f836b768c6ac1febd
rampage0131 <jake.docker0131@gmail.com>
2021-08-29 19:52:57 -0700

    feat(next.config.js): set default locale to `en-US`

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  i18n: { locales: ['en-US'], defaultLocale: 'en-US' },
   webpack(config, { isServer, defaultLoaders }) {
     if (!isServer && process.env.ANALYZE === 'true') {
       // Only run the bundle analyzer for the client-side chunks.
