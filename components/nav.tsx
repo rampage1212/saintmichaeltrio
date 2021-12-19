a6748ce4625bc62e1e36543064937c610344d4af
rampage0131 <jake.docker0131@gmail.com>
2021-12-19 14:13:45 -0800

    refactor(nav): consolidate `NavLink` definition

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,9 +1,74 @@
 import { useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
 import cn from 'classnames';
-
-import NavLink from 'components/nav-link';
+import { useRouter } from 'next/router';
 
 import { Callback } from 'lib/callback';
+
+interface NavLinkProps {
+  href: string;
+  children: string;
+  setActive?: Callback<{ x: number; width: number }>;
+}
+
+function NavLink({ href, children, setActive }: NavLinkProps): JSX.Element {
+  const { pathname } = useRouter();
+  const ref = useRef<HTMLLIElement>(null);
+  useEffect(() => {
+    if (pathname === href && ref.current && setActive)
+      setActive({ x: ref.current.offsetLeft, width: ref.current.clientWidth });
+  }, [pathname, href, setActive]);
+
+  return (
+    <li ref={ref}>
+      <Link href={href}>
+        <a
+          rel={!href.startsWith('/') ? 'noopener noreferrer' : undefined}
+          target={!href.startsWith('/') ? '_blank' : undefined}
+          className={cn({ active: pathname === href })}
+        >
+          {children}
+        </a>
+      </Link>
+      <style jsx>{`
+        li {
+          display: inline-block;
+          margin: 0 0.5rem;
+          float: none;
+        }
+
+        li:first-of-type {
+          margin-left: 0;
+        }
+
+        li:last-of-type {
+          margin-right: 0;
+        }
+
+        a {
+          display: block;
+          font-size: var(--link-size);
+          line-height: 24px;
+          padding: 0.5rem 0;
+        }
+
+        @media (max-width: 800px) {
+          li {
+            white-space: nowrap;
+          }
+
+          li:first-of-type {
+            margin-left: 0;
+          }
+
+          li:last-of-type {
+            margin-right: 1rem;
+          }
+        }
+      `}</style>
+    </li>
+  );
+}
 
 export interface NavProps {
   active: { x: number; width: number };
